f25ef5bd20c3cd3fda5ad9bc14bd68ab1c13ec1b
unknown <21522542@gm.uit.edu.vn>
2024-05-19 18:30:55 +0700

    change save_dir

--- KGAT-pytorch/parser/parser_nfm.py
+++ KGAT-pytorch/parser/parser_nfm.py
@@ -58,10 +58,11 @@ def parse_nfm_args():
 
     args = parser.parse_args()
 
-    # save_dir = 'trained_model/NFM/{}/{}_embed-dim{}_{}_lr{}_pretrain{}/'.format(
-    #     args.data_name, args.model_type, args.embed_dim,
-    #     '-'.join([str(i) for i in eval(args.hidden_dim_list)]), args.lr, args.use_pretrain)
-    save_dir = 'cf_scores.np'
+    save_dir = 'trained_model/NFM/{}/{}_embed-dim{}_{}_lr{}_pretrain{}_user-info{}/'.format(
+        args.data_name, args.model_type, args.embed_dim,
+        '-'.join([str(i) for i in eval(args.hidden_dim_list)]), args.lr, args.use_pretrain, 
+        args.use_user_info)
+
     args.save_dir = save_dir
 
     return args
