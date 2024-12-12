d8ba42da1f4a38320092944987892f3e20007b9d
SonRobo <143880885+SonRobo@users.noreply.github.com>
2024-12-12 19:55:03 +0700

    Update predict_kgat.py

--- KGAT-pytorch/predict_kgat.py
+++ KGAT-pytorch/predict_kgat.py
@@ -75,24 +75,65 @@ def predict_for_user(args, user_id):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--data_dir', type=str, required=True)
-    parser.add_argument('--data_name', type=str, default="")
-    parser.add_argument('--use_pretrain', type=int, default=0)
-    parser.add_argument('--pretrain_model_path', type=str, default="")
-    parser.add_argument('--n_epoch', type=int, default=6)
-    parser.add_argument('--cf_batch_size', type=int, default=1024)
-    parser.add_argument('--kg_batch_size', type=int, default=2048)
-    parser.add_argument('--test_batch_size', type=int, default=256)
-    parser.add_argument('--cf_print_every', type=int, default=500)
-    parser.add_argument('--kg_print_every', type=int, default=50)
-    parser.add_argument('--evaluate_every', type=int, default=2)
-    parser.add_argument('--Ks', type=str, default="[1, 5, 10]")
-    parser.add_argument('--save_dir', type=str, default="./results/")
+    parser.add_argument('--seed', type=int, default=2019,
+                        help='Random seed.')
+
+    parser.add_argument('--data_name', nargs='?', default='amazon-book',
+                        help='Choose a dataset from {yelp2018, last-fm, amazon-book}')
+    parser.add_argument('--data_dir', nargs='?', default='datasets/',
+                        help='Input data path.')
+
+    parser.add_argument('--use_pretrain', type=int, default=1,
+                        help='0: No pretrain, 1: Pretrain with the learned embeddings, 2: Pretrain with stored model.')
+    parser.add_argument('--pretrain_embedding_dir', nargs='?', default='datasets/pretrain/',
+                        help='Path of learned embeddings.')
+    parser.add_argument('--pretrain_model_path', nargs='?', default='trained_model/model.pth',
+                        help='Path of stored model.')
+
+    parser.add_argument('--cf_batch_size', type=int, default=1024,
+                        help='CF batch size.')
+    parser.add_argument('--kg_batch_size', type=int, default=2048,
+                        help='KG batch size.')
+    parser.add_argument('--test_batch_size', type=int, default=10000,
+                        help='Test batch size (the user number to test every batch).')
+
+    parser.add_argument('--embed_dim', type=int, default=64,
+                        help='User / entity Embedding size.')
+    parser.add_argument('--relation_dim', type=int, default=64,
+                        help='Relation Embedding size.')
+
+    parser.add_argument('--laplacian_type', type=str, default='random-walk',
+                        help='Specify the type of the adjacency (laplacian) matrix from {symmetric, random-walk}.')
+    parser.add_argument('--aggregation_type', type=str, default='bi-interaction',
+                        help='Specify the type of the aggregation layer from {gcn, graphsage, bi-interaction}.')
+    parser.add_argument('--conv_dim_list', nargs='?', default='[64, 32, 16]',
+                        help='Output sizes of every aggregation layer.')
+    parser.add_argument('--mess_dropout', nargs='?', default='[0.1, 0.1, 0.1]',
+                        help='Dropout probability w.r.t. message dropout for each deep layer. 0: no dropout.')
+
+    parser.add_argument('--kg_l2loss_lambda', type=float, default=1e-5,
+                        help='Lambda when calculating KG l2 loss.')
+    parser.add_argument('--cf_l2loss_lambda', type=float, default=1e-5,
+                        help='Lambda when calculating CF l2 loss.')
+
+    parser.add_argument('--lr', type=float, default=0.0001,
+                        help='Learning rate.')
+    parser.add_argument('--n_epoch', type=int, default=1000,
+                        help='Number of epoch.')
+    parser.add_argument('--stopping_steps', type=int, default=10,
+                        help='Number of epoch for early stopping')
+
+    parser.add_argument('--cf_print_every', type=int, default=1,
+                        help='Iter interval of printing CF loss.')
+    parser.add_argument('--kg_print_every', type=int, default=1,
+                        help='Iter interval of printing KG loss.')
+    parser.add_argument('--evaluate_every', type=int, default=10,
+                        help='Epoch interval of evaluating CF.')
+
+    parser.add_argument('--Ks', nargs='?', default='[20, 40, 60, 80, 100]',
+                        help='Calculate metric@K when evaluating.')
     parser.add_argument('--user_id', type=int, required=True, help="User ID to predict for")  # Thêm tham số user_id
-    parser.add_argument('--pretrain_embedding_dir', type=str)  
-    parser.add_argument('--laplacian_type', type=str, default='random-walk')  
-    parser.add_argument('--embed_dim', type=int, default=64)  
+
     args = parser.parse_args()
     predict_for_user(args, user_id=args.user_id)
 
